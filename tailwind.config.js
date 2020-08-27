d6c0ecc49149028fdedcfbaa0192b80e6f186ab0
Steven Steinwand <steven_ts@hotmail.com>
2020-08-27 17:06:35 -0600

    Font size changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,21 @@ module.exports = {
   purge: [],
   theme: {
     fontSize: {
-      'xs': '0.75rem', // 12px - Default
-      'sm': '0.875rem', // 14px - Default
-      'base': '1rem',  // 16px - Default
-      'lg': '1.125rem', // 18px - Default
-      'xl': '1.25rem', // 20px
-      '2xl': '1.375rem',  // 22px 
-      '3xl': '1.5rem', // 24px
-      '4xl': '1.625rem', // 26px
-      '5xl': '1.75rem', // 28px
-      '6xl': '2rem',  // 32px
-      '7xl': '2.5rem',  // 40px
-      '8xl': '3rem',  // 48px
-      '9xl': '3.75rem',  // 60px
-      '10xl': '4.5rem',  // 72px
-      '11xl': '6.25rem',  // 100px 
+      '12': '0.75rem', // 12px - Default
+      '14': '0.875rem', // 14px - Default
+      '16': '1rem',  // 16px - Default
+      '18': '1.125rem', // 18px - Default
+      '20': '1.25rem', // 20px
+      '22': '1.375rem',  // 22px 
+      '24': '1.5rem', // 24px
+      '26': '1.625rem', // 26px
+      '28': '1.75rem', // 28px
+      '32': '2rem',  // 32px
+      '40': '2.5rem',  // 40px
+      '48': '3rem',  // 48px
+      '60': '3.75rem',  // 60px
+      '72': '4.5rem',  // 72px
+      '100': '6.25rem',  // 100px 
     },
     container: {
       center: true,
